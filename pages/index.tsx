61438b58aa5657b3422dce47472e7bc8998678dd
Saoming <Hujanesword@gmail.com>
2021-10-13 17:18:27 +0700

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+function Homepage() {
+    return (
+      <h1 className="text-left my-24 text-black font-black tracking-tight text-6xl">Our homepage</h1>
+    )
+  }
+  
+  export default Homepage
+  
+  
